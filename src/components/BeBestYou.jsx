2ae9cbc37b6242a46a190722ba9933caa379c418
Chiranjeevi Makam <137588934+chiranjeevi-makam@users.noreply.github.com>
2024-02-19 11:08:17 +0530

    uodated_code

--- src/components/BeBestYou.jsx
+++ src/components/BeBestYou.jsx
@@ -8,10 +8,10 @@ const BeBestYou = () => {
         People with high emotional intelligence (EQ) live more fulfilled lives.
         They tend to behappier and have healtheir relationships.
       </div>
-      //<div className="text-xl p-4">
-       // They are more successful in their pursuits and make forinspiring
-       // leaders. According to science, they earns $29 a year
-     // </div>
+      <div className="text-xl p-4">
+        They are more successful in their pursuits and make forinspiring
+        leaders. According to science, they earns $29 a year
+      </div>
     </section>
   );
 };
